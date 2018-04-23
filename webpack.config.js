f2f7d8e4d46bf70244a2b465675bed3f9e37554e
lasacaJessica <lasaca@trilightphil.com>
2018-04-23 14:32:49 +0900

    Hot Module Replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
-  module.exports = {
-    entry: {
-           app: './src/index.js',
-           print: './src/print.js'
-         },
-         devtool: 'inline-source-map',
-         devServer: {
-            contentBase: './dist'
-             },
-         plugins: [
-          new CleanWebpackPlugin(['dist']),
-          new HtmlWebpackPlugin({
-          title: 'Development'
-          })
-          ],
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    //publicPath: '/'  //in the instruction but have to exclude due to when building problem occurred on the path adding '/' to bundle.js in index.html
-    },
-   };
+module.exports = {
+  entry: {
+      app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+     hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement'
+    }),
+     new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
