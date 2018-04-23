fb6a4a8408115344cf910aff4bdf88085b11f586
lasacaJessica <lasaca@trilightphil.com>
2018-04-23 17:32:05 +0900

    Caching

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import _ from 'lodash';
+// import Print from './print';
 
-function component() {
+async function getComponent() {
+ 
   var element = document.createElement('div');
-  var button = document.createElement('button');
-  var br = document.createElement('br');
 
-    button.innerHTML = 'Click me and look at the console!';
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.appendChild(br);
-    element.appendChild(button);
+   // element.onclick = Print.bind(null, 'Hello webpack!');
+
+    return element;
+    }
   
-    // Note that because a network request is involved, some indication
-    // of loading would need to be shown in a production-level site/app.
-  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-  var print = module.default;
-  print();
-  });
-
-  return element;
-  }
-
     document.body.appendChild(component());
