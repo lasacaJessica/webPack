4565c315a9aa7a8daf121fba7a67ef35af3a69a3
lasacaJessica <lasaca@trilightphil.com>
2018-04-23 15:01:32 +0900

    Tree Shaking

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import { cube } from './math.js';
 
+  function component() {
+    var element = document.createElement('pre');
+    
+    element.innerHTML = ['Hello webpack!','5 cubed is equal to ' + cube(5)].join('\n\n');
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+    return element;
+  }
 
-  element.innerHTML = _.join(['Hello', 'webpack \n'], ' ');
-
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;  // onclick event is bind to the original printMe function
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-let element = component();
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click handler
-    document.body.appendChild(element);
-  })
-}
+  document.body.appendChild(component());
